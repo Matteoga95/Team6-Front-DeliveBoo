23fdbca93cfd9c5998fa6a436664d949dd1c3963
paoloevan <paoloev1900@gmail.com>
2023-02-15 12:32:30 +0100

    fix style RestaurantView

--- src/views/RestaurantView.vue
+++ src/views/RestaurantView.vue
@@ -159,7 +159,7 @@ export default {
 
 <template>
     <section class="">
-        <a @click="getBack()">back</a>
+        <!-- <a @click="getBack()">back</a> -->
         <div class="container ">
 
             <!-- The Modal -->
@@ -181,79 +181,80 @@ export default {
 
 
             <!-- banner con la foto del ristorante -->
-            <div class="d-flex justify-content-center">
-                <img class="mb-4 cover" src="https://www.travel365.it/foto/altitude-at-shangri-la.jpg" alt="">
-            </div>
-
-
-            <!-- info del ristorante -->
-            <div class="d-flex justify-content-center text-center">
-
-                <div class="">
-                    <h1 class="mb-4">{{ this.restaurant.name }}</h1>
-                    <div class="d-flex justify-content-center" v-for="types in this.restaurant.tipologies">
-                        <p class="type">{{ types.name }} </p>
-                    </div>
-                    <div class="">
-                        <div class="d-flex">
-                            <font-awesome-icon icon="fa-solid fa-location-dot" />
-                            <h2 class="px-3">{{ this.restaurant.address }}</h2>
-
-                        </div>
-                        <div class="d-flex">
-                            <font-awesome-icon icon="fa-solid fa-phone" />
-                            <h2 class="px-3"> {{ this.restaurant.phone_number }}</h2>
+            <div class="row">
+                <div class="col-3  restaurants">
+                    <div class="my-card">
+                        <div class="d-flex justify-content-center">
+                            <img class="mb-4 cover" :src="state.imagePath(restaurant.cover_image)" alt="">
                         </div>
 
+                        <!-- info del ristorante -->
+                        <div class="d-flex justify-content-center text-center">
 
+                            <div class="">
+                                <h1 class="">{{ this.restaurant.name }}</h1>
+                                <div class="d-flex justify-content-center">
+                                    <div v-for="types in this.restaurant.tipologies" class="type">{{ types.name }}</div>
+                                </div>
+                                <div class="">
+                                    <div class="d-flex my-3">
+                                        <font-awesome-icon icon="fa-solid fa-location-dot" />
+                                        <div class="px-3">{{ this.restaurant.address }}</div>
+
+                                    </div>
+                                    <div class="d-flex my-3">
+                                        <font-awesome-icon icon="fa-solid fa-phone" />
+                                        <div class="px-3"> {{ this.restaurant.phone_number }}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-
                 </div>
-
-
-
-
-            </div>
-
-            <!-- piatti e cart-->
-            <div class="d-flex justify-content-between">
 
                 <!-- piatti -->
-                <div class="col-10" v-if="this.has_dishes">
-                    <div v-for="dish in this.restaurant.dishes">
-                        <div class="my-card d-flex my-5">
-                            <img width="200" :src="state.imagePath(dish.cover_image)" alt="">
-                            <div class="details p-3 flex-grow-1">
-                                <h1>{{ dish.name }}</h1>
-                                <p>{{ dish.description }}</p>
-                                <h2 class="pr-3"> {{ dish.price }} <span>&#8364;</span></h2>
-                            </div>
-                            <div class="align-self-center text-end">
-                                <button @click="addDishToCart(dish)"
-                                    class="btn py-2 px-3 mx-4 btn-primary d-flex align-items-center">
-                                    Add to cart
-                                </button>
+                <div class="col-6 d-flex justify-content-center">
+                    <div class="col-10" v-if="this.has_dishes">
+                        <div v-for="dish in this.restaurant.dishes">
+                            <div class="my-card d-flex p-1 my-3">
+                                <img width="200" :src="state.imagePath(dish.cover_image)" alt="">
+                                <div class="details p-3 flex-grow-1">
+                                    <h2>{{ dish.name }}</h2>
+                                    <p>{{ dish.description }}</p>
+                                    <h4 class="pr-3"> {{ dish.price }} <span>&#8364;</span></h4>
+                                </div>
+                                <div class="align-self-center text-end">
+                                    <button @click="addDishToCart(dish)"
+                                        class="btn py-2 px-3 mx-4 btn-primary d-flex align-items-center">
+                                        Add to cart
+                                    </button>
+                                </div>
+
                             </div>
 
                         </div>
-
                     </div>
+                    <h2 class=" py-5 text-center" v-else>There are no dishes for this restaurant yet</h2>
+
                 </div>
-                <h2 class=" py-5 text-center" v-else>There are no dishes for this restaurant yet</h2>
 
                 <!-- cart -->
-                <div class="mx-5 px-5 col-4">
-                    <h2 class="text-center mx-5">Cart</h2>
-                    <div class="d-flex justify-content-start" v-for="dish in this.cart">
-                        <h2 class="my-4">{{ dish.name }}</h2>
-                        <button type="button" @click="removeDishToCart(dish)" class=" my-4 mx-3 btn btn-danger">
-                            <font-awesome-icon icon="fa-solid fa-minus" />
-                        </button>
+                <div class="col-3">
+                    <div class="my-card">
+                        <div class="mx-5 px-5 col-4">
+                            <h2 class="text-center mx-5">Cart</h2>
+                            <div class="d-flex justify-content-start" v-for="dish in this.cart">
+                                <h2 class="my-4">{{ dish.name }}</h2>
+                                <button type="button" @click="removeDishToCart(dish)" class=" my-4 mx-3 btn btn-danger">
+                                    <font-awesome-icon icon="fa-solid fa-minus" />
+                                </button>
 
+                            </div>
+                        </div>
                     </div>
                 </div>
-
             </div>
+
 
         </div>
     </section>
@@ -268,6 +269,43 @@ export default {
     --main-color: #e29436;
     --main-color-dark: #cf4835;
 
+}
+
+.my-card {
+    padding: 1.5rem;
+    box-shadow: 0 0 3px 0px rgb(192, 192, 192);
+    border-radius: 5px;
+
+    .tipology {
+        color: #e29436;
+    }
+
+    .type {
+        color: var(--main-color-dark);
+    }
+
+    img {
+        border-radius: 5px;
+        object-fit: cover;
+        object-position: center;
+        height: 13rem;
+    }
+
+}
+
+.restaurants {
+    img {
+        border-radius: 5px;
+        object-fit: cover;
+        object-position: center;
+        height: 20rem;
+    }
+}
+
+.my-btn {
+    border-radius: 30px;
+    padding-left: 1rem;
+    padding-right: 1rem;
 }
 
 .modal {
@@ -329,26 +367,5 @@ export default {
 
 .type {
     padding: 5px;
-}
-
-.my-card {
-    padding: 1.5rem;
-    box-shadow: 0 0 6px 0px rgb(192, 192, 192);
-    border-radius: 5px;
-
-    .tipology {
-        color: #e29436;
-    }
-
-    img {
-        border-radius: 5px;
-
-    }
-
-    .my-btn {
-        border-radius: 30px;
-        padding-left: 1rem;
-        padding-right: 1rem;
-    }
 }
 </style>
